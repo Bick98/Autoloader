27a268d5da65f4a5cbcf6a9e2f463b2c79c3021c
Egor <85055085+Bick98@users.noreply.github.com>
2022-06-05 19:56:49 +1000

    Add files via upload

---
+++ Classes/Class1.php
@@ -0,0 +1,7 @@
+<?php
+class Class1{
+    public function getClass1(){
+        echo 'Класс номер 1' . PHP_EOL;
+    }
+}
+?>
---
+++ Classes/SubClasses/Class2.php
@@ -0,0 +1,8 @@
+<?php
+namespace SubClasses;
+class Class2{
+    public function getClass2(){
+        echo 'Класс номер два' . PHP_EOL;
+    }
+}
+?>
